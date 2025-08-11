cb5bb71c4b78f317615160cde2b130666697f2e2
pabloT0807 <pabloterrazas0807@gmail.com>
2025-08-11 16:13:15 -0600

    first commit1

--- app.js
+++ app.js
@@ -27,7 +27,31 @@ function agregarAmigo(){
 
 //actulizarf la lista 
 
+function actualizarLista(){
+    const listaAmigos = document.getElementById('listaAmigos');
 
+    //limpiar el contenido actual 
+    listaAmigos.innerHTML = "";//borradno contenido previo
+
+    //se recorre el ararry
+    for(let i = 0;i<amigos.length;i++){
+        const li = document.createElement('li');
+        li.textContent = amigos[i];
+        listaAmigos.appendChild(li);
+    }
+}
 //seleccionar un amigo aleatorio
+function sortearAmigo(){
+    //validando amigos disponibles
+    if(amigos.length == 0 ){
+        alert("no hay amigos disponibles");
+        return;
+    }
+    const indiceAle = Math.floor(Math.random()  * amigos.length)
 
-//
+    //detener el nombr esorteado
+    const amigosSort =  amigos[indiceAle];
+
+    const resultado = document.getElementById('Resultado');
+    resultado.innerHTML =  `Amigo sorteado: <strong>${amigosSort}</strong>`; 
+}
